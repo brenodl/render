19e08e9ebba841b98357b9880cff62be2864747e
brenodl <brenodelgadolima@gmail.com>
2025-10-10 07:42:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import streamlit as st
 
 st.header('Jogando uma moeda')
@@ -9,3 +10,4 @@ if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
 
 st.write('Ainda não é um aplicativo funcional. Em construção.')
+
